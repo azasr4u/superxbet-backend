db61ac91c9a0a93727b3a442501330742f9aee84
azasr4u <azas.rokz@email.com>
2026-03-29 04:23:41 +0530

    final admin panel update with models restored

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import multer from "multer";
 
 import { verifyToken } from "../middleware/auth.js";
 import { adminOnly, agentOrAdmin } from "../middleware/roles.js";
