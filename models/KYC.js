4a9d53d1b122b2f00217c56f161377498b1e494e
azasr4u <72634911+azasr4u@users.noreply.github.com>
2026-03-29 01:05:38 +0530

    Delete models directory

--- models/KYC.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const schema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  document: String,
-  status: { type: String, default: "Pending" }
-});
-
-export default mongoose.model("KYC", schema);
--- models/Withdraw.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const withdrawSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  amount: Number,
-  status: { type: String, default: "Pending" }
-}, { timestamps: true });
-
-export default mongoose.model("Withdraw", withdrawSchema);
