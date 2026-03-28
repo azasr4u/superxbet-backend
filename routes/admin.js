0a1c709b3efd75dd4c5b98dccf1bbf24ae19e7ed
azasr4u <azas.rokz@email.com>
2026-03-29 04:48:16 +0530

    fix multer not defined error

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import multer from "multer";
 
 import { verifyToken } from "../middleware/auth.js";
 import { adminOnly, agentOrAdmin } from "../middleware/roles.js";
